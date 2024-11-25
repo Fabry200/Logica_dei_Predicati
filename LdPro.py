e03eab5cf24d40463eca9a0855f2299ffeeaa3bf
Fabrizio Verace <39920892+Fabry200@users.noreply.github.com>
2024-11-25 16:51:28 +0100

    Update LdPro.py

--- LdPro.py
+++ LdPro.py
@@ -1,4 +1,5 @@
 #autore fabrizio verace I.A 2024
+# @title Soluzione con DNF
 from itertools import product
 
 class Nodo:
@@ -10,7 +11,6 @@ class Nodo:
 
   def stampa(self, livello=0): #funzione di stampa, suggerita dal prof
     spazio=" "*livello+self.name+"\n"
-
     if self.left:
       spazio+=self.left.stampa(livello+1)
 
@@ -50,7 +50,7 @@ class Nodo:
 
     dictb={'A': ' and ', 'O': ' or ', 'N': 'not '}
     counter=0
-    for x in range(len(self.p)):  
+    for x in range(len(self.p)):
       for j in range(len(self.p[0])):
          dictb[lista_atomi_presenti[counter]]=self.p[x][j]  #itero attraverso il prodotto cartesiano e assegno a dictb[atomo] il valore associato al prodotto cartesiano eseguito
          self.tabella_verita[x].append(self.p[x][j])
@@ -70,12 +70,12 @@ class Nodo:
     else:
       return self.valori_verita
 
-  def is_tautologia(self,n): 
-    for x in self.tabella(n): 
+  def is_tautologia(self,n):
+    for x in self.tabella(n):
       if x == False: #tutti true
         return False
     return True
-   
+
   def is_soddisfacibile(self,n):
     if self.tabella(n,False).count(True) != 0 : #almeno un true
       return True
@@ -95,7 +95,7 @@ class Nodo:
 
   def calcola_equivalente(self):
 
-    def distribuitiva(): 
+    def distribuitiva():
       atomi=['p', 'q', 'r', 's']
       lista_duplicati=[]
       preposizione=None
@@ -138,6 +138,47 @@ class Nodo:
 
     equivalente=distribuitiva()
     return equivalente
+
+  def dnf(self, numero_atomi):  #Forma normale disgiuntiva
+    self.tabella(numero_atomi, scrittura=False)
+    self.dnf_table=[[] for x in range(numero_atomi*numero_atomi)]
+    self.dnf_preposition=[]
+
+    lista_atomi=['s','p','q','r']
+    lista_atomi_presenti=set([x for x in self.name if x in lista_atomi]) #prima converto in set cosi elimino le ripetizioni di elementi poi in lista, cosi lavoro agevolmente
+    lista_atomi_presenti=list(lista_atomi_presenti)
+    indice_atomi=0
+
+    counter=0
+    for rows in self.tabella_verita:
+      stringa=self.name
+      for value in rows[:numero_atomi]:
+        if value == 'False':
+          self.dnf_table[counter].append(f'N{value}')
+          stringa=stringa.replace(lista_atomi_presenti[indice_atomi-1],f'N{lista_atomi_presenti[indice_atomi-1]}')
+        else:
+          self.dnf_table[counter].append(f'{value}')
+        indice_atomi+=1
+    
+      stringa=stringa.replace('A', ' and  ').replace('N','not ').replace('O', ' or ')
+      stringa=Nodo(stringa)
+      self.dnf_preposition.append(stringa)
+      indice_atomi=0
+      counter+=1
+    for rows in self.dnf_table:
+      print(rows)
+
+    stringa=' or '.join(rows.name for rows in self.dnf_preposition)
+    stringa=Nodo(stringa)
+
+    stringa_copia=stringa.name.replace(' and ', 'A').replace('not ','N').replace(' or ', 'O')
+    stringa_copia=Nodo(stringa_copia)
+    dizionario ={'p': False, 'q': False, 'r': False, 'A': ' and ', 'O': ' or ', 'N': 'not '} #1) dizionario per il primo caso
+    print(stringa_copia,'-->', stringa_copia.values(dizionario))
+    
+    return stringa
+
+    
 
 
 def costruisci(albero, nodo):
@@ -184,7 +225,7 @@ def is_equal(p1,n1, p2, n2):
 def main():
   A='((p and q) and (not r))'.replace(' and ', 'A').replace('not ','N').replace(' or ', 'O')
   B='((r or q) and (not r))'.replace(' and ', 'A').replace('not ','N').replace(' or ', 'O')
-  C='(p or (not p))'.replace(' and ', 'A').replace('not ','N').replace(' or ', 'O')
+  C='(p or (not q))'.replace(' and ', 'A').replace('not ','N').replace(' or ', 'O')
   D='((p or q) and (p or r))'.replace(' and ', 'A').replace('not ','N').replace(' or ', 'O')
 
   p1=Nodo(A)
@@ -193,7 +234,6 @@ def main():
   p4=Nodo(D)
 
   dizionario ={'p': True, 'q': True, 'r': False, 'A': ' and ', 'O': ' or ', 'N': 'not '} #1) dizionario per il primo caso
- 
   #print(p1.values(dizionario)) #1) caso, calcolare VdV per qualche valore che abbiamo assegnato (assegnazione manuale tramite dizionario)
   #print(p1.tabella(3)) #2) caso, calcolare la tabella di verita' di una proposizione (la funzione tabella returna la colonna dei VdV, ma la tabella diventa comunque un attributo della classe.)
   #print(p2.tabella(2)) #Bisogna specificare quanti atomi non ripetuti ci sono
@@ -202,8 +242,8 @@ def main():
   #print(p3.is_tautologia(1))  #3.1)Tautologia
   #print(p3.is_soddisfacibile(1)) #3.2)Soddisfacibile
   #print(p2.is_notsoddisfacibile(2)) #3.3)Non soddisfacibile
-   #print(p1.is_falsificabile()) #3.4)Falsificabile
-  #is_equal(p1,3,p2,2) 4) Calcolo se una proposizione e' equivalente
+  #print(p1.is_falsificabile(3)) #3.4)Falsificabile
+  #is_equal(p1,3,p2,2) #4) Calcolo se una proposizione e' equivalente
 
   #4) Calcolo della preposizione equivalente solo in un caso (come richiesto dal prof), con la proprieta dei predicati distribuitiva
   #print(p4.calcola_equivalente())
@@ -214,6 +254,10 @@ def main():
 
   #costruzione del albero
   #costruisci(A,p1)
+  #print(p1)
+
+  #costruzione della forma normale disgiuntiva (numero atomi da specificare)
+  #print(p3.dnf(2))
 
 if __name__ == "__main__":
   main()
